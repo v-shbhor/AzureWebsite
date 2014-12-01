de036ede6ef4b416844d8bd9804730117da31352
Shailesh <shabho@live.com>
2014-12-01 09:15:00 +0000

    Code for enabling diagnostics and view application trace logs

--- MyExample/Controllers/HomeController.cs
+++ MyExample/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Diagnostics;
+
 
 namespace MyExample.Controllers
 {
@@ -10,21 +12,30 @@ namespace MyExample.Controllers
     {
         public ActionResult Index()
         {
+            Trace.WriteLine("Entering Index method");
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            Trace.TraceInformation("Displaying the Index page at " + DateTime.Now.ToLongTimeString());
+            Trace.WriteLine("Leaving Index method");
             return View();
         }
 
         public ActionResult About()
         {
             string currentTime = DateTime.Now.ToLongTimeString();
+            Trace.WriteLine("Entering About method");
             ViewBag.Message = "The current time is " + currentTime;
+            ViewBag.Message = "Your app description page.";
+            Trace.TraceWarning("Transient error on the About page at " + DateTime.Now.ToShortTimeString());
+            Trace.WriteLine("Leaving About method");
             return View();
-
         }
 
         public ActionResult Contact()
         {
+            Trace.WriteLine("Entering Contact method");
             ViewBag.Message = "Your contact page.";
-
+            Trace.TraceError("Fatal error on the Contact page at " + DateTime.Now.ToLongTimeString());
+            Trace.WriteLine("Leaving Contact method");
             return View();
         }
     }
