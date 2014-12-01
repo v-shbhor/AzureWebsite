4fc9128da75b0b5b90349991c9f315742ba61f6c
Shailesh <shabho@live.com>
2014-12-01 08:00:46 +0000

    Created

---
+++ MyExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyExample.Startup))]
+namespace MyExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
